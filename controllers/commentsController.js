040981068444427f0a9fffc68dc7c4613f23a683
darriub15 <darriusabrown@gmail.com>
2022-04-18 14:20:21 -0700

    All routes working with EJS

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -6,27 +6,9 @@ const router = express.Router()
 
 
 
-router.delete('/:episode/:id', (req, res) => {
-    console.log('Delete Comment route reached!')
-    console.log(req.params.id)
-    Comment.findOneAndDelete({
-        _id: req.params.id},
-    () => {
-        res.redirect('/episode')
-    })
-  })
 
-router.put('/:episode/:id', (req, res) => {
-    console.log('Edit Comment route reached!')
-    Comment.findOneAndUpdate({
-        _id: req.params.id},
-        req.body,
-        {new: true})
-    .then(() => {
-        res.redirect('/episode')
-    })
-    .catch(console.error);
-    });
+
+
 
 const CommentsController = router
 module.exports = CommentsController;
--- controllers/episodesController.js
+++ controllers/episodesController.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Episode = require('../models/episodes-model')
 const Comment = require('../models/comments-model');
-
+//READ
 router.get("/", (req, res, next) => {
   console.log('Get episodes route reached!')
     Episode.find({})
@@ -26,7 +26,7 @@ router.post("/", (req, res) => {
     Episode.create(req.body)
     .then(res.redirect("/"))
   });
-
+//CREATE COMMENT
   router.get('/:episode/:new', (req, res, next) => {
     Episode.find({episodeNumber:req.params.episode})
     .then(ep => {
@@ -48,6 +48,38 @@ router.post("/:episode", (req, res) => {
 })
 .catch(console.error)
 })
+
+router.get('/:episode/:id/:edit', (req, res, next) => {
+  Episode.find({episodeNumber:req.params.episode})
+  .then(ep => {
+    Comment.find({_id:req.params.id})
+  .then((comment) => {
+    res.render("view/edit", {episode: ep, comments: comment})})
+  })
+  .catch(next);
+});
+
+router.put('/:episode/:id', (req, res) => {
+  console.log('Edit Comment route reached!')
+  Comment.findOneAndUpdate({
+      _id: req.params.id},
+      req.body,
+      {new: true})
+  .then(() => {
+      res.redirect('/')
+  })
+  .catch(console.error);
+  });
+
+  router.delete('/:episode/:id', (req, res) => {
+    console.log('Delete Comment route reached!')
+    console.log(req.params.id)
+    Comment.findOneAndDelete({
+        _id: req.params.id},
+    () => {
+        res.redirect('/')
+    })
+  })
 
 
   const EpisodesController = router
