bba02a3f5c2ba5bdececc738a54a1755b90675b1
darriub15 <darriusabrown@gmail.com>
2022-04-20 17:14:20 -0700

    after office hours progress

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,85 +5,96 @@ const Comment = require('../models/comments-model');
 //READ
 router.get("/", (req, res, next) => {
   console.log('Get episodes route reached!')
-    Episode.find({})
-      .then(episode => {
-        res.render("view/main", {episodes: episode})})
-      .catch(next);
-  });
+  Episode.find({})
+    .then(episode => {
+      res.render("view/main", { episodes: episode })
+    })
+    .catch(next);
+});
 
 router.get("/:episode", (req, res, next) => {
   console.log('Get episode route reached!')
-Episode.find({episodeNumber:req.params.episode})
-    .then(ep => {
-      Comment.find({episode:req.params.episode}).sort({updatedAt:-1})
-    .then((comment) => {
-      res.render("view/episode", {episode: ep, comments: comment})})
+  Episode.find({ episodeNumber: req.params.episode })
+    .then(episode => {
+      Comment.find({ episode: req.params.episode }).sort({ updatedAt: -1 })
+        .then((comments) => {
+          res.render("view/episode", {episode, comments })
+        })
     })
     .catch(next);
 });
 
 
 //CREATE COMMENT
-  router.get('/:episode/:new', (req, res, next) => {
-    Episode.find({episodeNumber:req.params.episode})
+router.get('/:episode/:new', (req, res, next) => {
+  Episode.find({ episodeNumber: req.params.episode })
     .then(ep => {
-      Comment.find({episode:req.params.episode})
-    .then((comment) => {
-      res.render("view/new", {episode: ep, comments: comment})})
+      Comment.find({ episode: req.params.episode })
+        .then((comment) => {
+          res.render("view/new", { episode: ep, comments: comment })
+        })
     })
     .catch(next);
 });
 
-router.post("/:episode", (req, res) => {
+router.post("/episode/:id", (req, res) => {
   console.log('Add comment route reached!')
+  console.log(req.body.post)
+  console.log(req.params.id)
   Comment.create(
-    req.body
-)
-.then((post) => {
-  console.log(post)
-    res.redirect(`/`)
-})
-.catch(console.error)
+    {
+      post: req.body.post,
+      episode: req.params.id
+    }
+  )
+    .then((post) => {
+      console.log(req.body.episode)
+      res.redirect(`/${req.params.id}`)
+    })
+    .catch(console.error)
 })
 //EDIT COMMENT
 router.get('/:episode/:id/:edit', (req, res, next) => {
-  Episode.find({episodeNumber:req.params.episode})
-  .then(ep => {
-    Comment.find({_id:req.params.id})
-  .then((comment) => {
-    res.render("view/edit", {episode: ep, comments: comment})})
-  })
-  .catch(next);
+  Episode.find({ episodeNumber: req.params.episode })
+    .then(ep => {
+      Comment.find({ _id: req.params.id })
+        .then((comment) => {
+          res.render("view/edit", { episode: ep, comments: comment })
+        })
+    })
+    .catch(next);
 });
 
 router.put('/:episode/:id', (req, res) => {
   console.log('Edit Comment route reached!')
   Comment.findOneAndUpdate({
-      _id: req.params.id},
-      req.body,
-      {new: true})
-  .then(() => {
-      res.redirect('/')
-  })
-  .catch(console.error);
-  });
+    _id: req.params.id
+  },
+    req.body,
+    { new: true })
+    .then(() => {
+      res.redirect(`/${req.params.episode}`)
+    })
+    .catch(console.error);
+});
 
 //DELETE COMMENT
-  router.delete('/:episode/:id', (req, res) => {
-    console.log('Delete Comment route reached!')
-    console.log(req.params.id)
-    Comment.findOneAndDelete({
-        _id: req.params.id},
+router.delete('/:episode/:id', (req, res) => {
+  console.log('Delete Comment route reached!')
+  console.log(req.params.id)
+  Comment.findOneAndDelete({
+    _id: req.params.id
+  },
     () => {
-        res.redirect('/')
+      res.redirect(`/${req.params.episode}`)
     })
-  })
+})
 
 //CREATE EPISODE (rare)
-  router.post("/", (req, res) => {
-    Episode.create(req.body)
+router.post("/", (req, res) => {
+  Episode.create(req.body)
     .then(res.redirect("/"))
-  });
+});
 
-  const EpisodesController = router
-  module.exports = EpisodesController;
+const EpisodesController = router
+module.exports = EpisodesController;
--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const app = express()
 const methodOverride = require('method-override')
 const cors = require('cors')
 const EpisodesControllers = require('./controllers/controller')
+const layout = require('express-ejs-layouts')
 
 app.use(cors())
+app.use(layout)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+//express.static
 app.use(express.static('public'))
 app.set("view engine", "ejs")
 app.use('', EpisodesControllers)
